2c2b356dc1f3de5b98ba90bdcc987e6e55af47fb
Joao Pedro <developerjotape@gmail.com>
2025-04-04 17:06:48 -0300

    feat: rota de login adicionada

--- domains/users/routes.js
+++ domains/users/routes.js
@@ -35,4 +35,32 @@ router.post("/", async (req, res) => {
   }
 });
 
+router.post("/login", async (req, res) => {
+  connectDb();
+
+  const { email, password } = req.body;
+  try {
+    /* Verifica se o usuário existe */
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res
+        .status(401)
+        .json({ error: "Acesso negado: Usuário não encontrado!" });
+    }
+
+    const { _id, name } = user;
+
+    // Testa se a senha bate
+    const passwordMatch = bcrypt.compareSync(password, user.password);
+    if (!passwordMatch) {
+      return res.status(401).json({ error: "Acesso negado: Senha incorreta!" });
+    }
+
+    /* Caso der tudo certo, retorna o usuário */
+    res.json({ _id, name, email });
+  } catch (error) {
+    res.status(404).json({ error: "Erro ao buscar usuário" });
+  }
+});
+
 export default router;
--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 import "dotenv/config";
 import express from "express";
 import UserRoutes from "./domains/users/routes.js";
+import cors from "cors";
 
 const app = express();
 const { PORT } = process.env;
 
 app.use(express.json());
+app.use(cors());
 app.use("/users", UserRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
-
-// 0662DMnU6GtGB7XM
